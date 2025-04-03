d4a849a11802332239b7f4b7fd14266faf30f344
Andreea <106435918+DanAndreeaMaria@users.noreply.github.com>
2025-04-03 12:13:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,50 @@
 function myFunction() {
   var x = document.getElementById("myTopnav");
-  var menuIcon = document.querySelector(".icon ion-icon");
+  var menuIcon = document.querySelector(".icon .material-symbols-rounded");
 
-  if (x.className === "topnav") {
-    x.className += " responsive";
-    menuIcon.setAttribute("name", "close-outline"); // Change to X icon
+  if (x.classList.contains("responsive")) {
+    x.classList.remove("responsive");
+    menuIcon.textContent = "menu"; // Switch back to menu icon
   } else {
-    x.className = "topnav";
-    menuIcon.setAttribute("name", "menu-outline"); // Change back to hamburger
+    x.classList.add("responsive");
+    menuIcon.textContent = "close"; // Switch to close icon
   }
 }
 
 // Function that closes the menu when clicking outside of it
 document.addEventListener("click", function (event) {
   var x = document.getElementById("myTopnav");
-  var menuButton = document.querySelector(".icon");
+  var menuButton = document.querySelector(".icon .material-symbols-rounded");
   var isClickInsideMenu = x.contains(event.target); // Check if the click is inside the menu
   var isClickOnButton = menuButton.contains(event.target); // Check if the click is on the menu button
 
   // If the click is outside the menu and the button, close the menu
   if (!isClickInsideMenu && !isClickOnButton) {
-    x.className = "topnav"; // Close the menu
-    var menuIcon = document.querySelector(".icon ion-icon");
-    menuIcon.setAttribute("name", "menu-outline"); // Change the hamburger
+    x.classList.remove("responsive"); // Close the menu
+    var menuIcon = document.querySelector(".icon .material-symbols-rounded");
+    menuIcon.textContent = "menu"; // Change back to menu icon
   }
 });
+
+// Go back to top button function
+// Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll back to top
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
